0efe5552fa5dbd8dd731a6210f150a1bace167a8
CarlaDCyr <118022663+CarlaDCyr@users.noreply.github.com>
2022-12-25 22:15:03 -0700

    Finished out styling

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-//*************connect html elements to js
+//Connecting HTML to JS
 const title = document.getElementById('title');
 const search = document.getElementById('findByIngredient');
 const ingredient = document.getElementById('cocktailIngredient');
 const getRandom = document.getElementById('randomButton');
 const drinkSection = document.getElementById('drink-section');
 
-// name the URLs here to make it cleaner in the functions
+// API URLS
 const randomURL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 const ingredientURL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
 const idURL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
-
-// UTILITY FUNCTIONS // UTILITY FUNCTIONS // UTILITY FUNCTIONS //
 
 
 function checkStatus(response) {
@@ -24,7 +22,6 @@ function checkStatus(response) {
   
   
   function clearBar() {
-    // Remove current children of drink section
     while (drinkSection.firstChild) {
       drinkSection.removeChild(drinkSection.lastChild);
     }
@@ -32,18 +29,9 @@ function checkStatus(response) {
   
   
   function addCocktail(data) {
-    // data will have the structure { drinks: Object[] }
-    // Only one cocktail will be present, so we simply choose the
-    // first object in the drinks array
     displayCocktail(data.drinks[0]);
   }
 
-
-
-  // RANDOM COCKTAIL // RANDOM COCKTAIL // RANDOM // RANDOM COCKTAIL //
-
-
-//*************get the API and iterate through the objects to display the results if fail then catch it and display the error********
 function getRandomCocktail() {
     fetch(randomURL)
     .then(checkStatus)
@@ -51,25 +39,22 @@ function getRandomCocktail() {
         .catch(function (err) {
             console.log('There was an error:', err);
         });
-    // ****** clear the bar before adding the new cocktail *********
+
     function replaceCurrentCocktail(data) {
         clearBar ()
         addCocktail(data)
     }
 }
 
-// FIND BY INGREDIENT // FIND BY INGREDIENT // FIND BY INGREDIENT //
-
 function findByIngredient() {
     const chosenIngredient = ingredient.value
     if (!chosenIngredient) {
       return
     } else if (chosenIngredient.indexOf(",") > -1) {
-      // You need to be a $2+ Patreon supporter to search for multi-ingredients so i can only show you the first one
       return
     }
     
-    // ********* replace the current cocktail with the new one *********
+
    const searchURL = `${ingredientURL}${chosenIngredient}`;
     fetch(searchURL)
     .then(checkStatus)
@@ -79,20 +64,10 @@ function findByIngredient() {
         });
  /**
    *
-   * @param {Object} data has the format:
-   * { "drinks": [
-   *     {
-   *       "strDrink": "Brandy Flip",
-   *       "strDrinkThumb": "https://www.thecocktaildb.com/images/...jpg",
-   *       "idDrink": "11164"
-   *     },
-   *     ...
-   *   ]
-   * }
+   * @param {Object} data
    */
 
   function replaceCurrentCocktails(data) {
-    // console.log("data", JSON.stringify(data, null, '  '));
     clearBar()
     data.drinks.forEach(getCocktailById);
   }
@@ -107,13 +82,7 @@ function findByIngredient() {
   }
 }
 
-
-// DISPLAY COCKTAIL // DISPLAY COCKTAIL / DISPLAY / DISPLAY COCKTAIL //
-
-// ***** display the information about the cocktail
 function displayCocktail(cocktail) {
-    //console.log("cocktail", JSONstringify(cocktail, null,' '));
-    // Add h2 title with name of the cocktail
     const drinkName = document.createElement('h2');
     drinkName.innerHTML = cocktail.strDrink;
     drinkSection.appendChild(drinkName);
@@ -125,31 +94,24 @@ function displayCocktail(cocktail) {
     const ul = document.createElement('ul');
     drinkSection.appendChild(ul)
 
-// ******* there are  UPTO 15 ingredients in the cocktail therefor loop through 15 times only *********
     for (let i = 1; i < 16; i++) {
-        //if there are no more ingredients, break out of the loop
         const ingredient = cocktail[`strIngredient${i}`];
         if (!ingredient) {
             break;
         }
-        // Some ingredients like "egg" don't have any quanity, so we need
-        // to account for that
         let quantity = cocktail[`strMeasure${i}`] || "";
         if (quantity) {
             quantity = ": " + quantity
         }
-        // create a <li> for each ingredient and the quantity and append it to the <ul>
         const ingredientLI = document.createElement("li");
         ingredientLI.innerText = ingredient + quantity;
         ul.appendChild(ingredientLI);
     }
-//  ******* add the instructions to the page *********
+
     let instructions = document.createElement("p");
     instructions.innerHTML = cocktail.strInstructions;
     drinkSection.appendChild(instructions);
 }
-
-// KEYBOARD SHORTCUT // KEYBOARD SHORTCUT // KEYBOARD SHORTCUT //
 
 function checkForEnter(event) {
     if (event.key === "Enter") {
@@ -158,10 +120,8 @@ function checkForEnter(event) {
     }
   }
   
-  // Activate the buttons
   search.addEventListener("click", findByIngredient)
   ingredient.addEventListener("keyup", checkForEnter)
 
-  // Open the page with a random cocktail by default
 getRandomCocktail();
 getRandom.addEventListener("click", getRandomCocktail)
